7ffe5cc94f2a9de1dc4c63a41cc2513058652645
kaito-a-bit <arakai0128@gmail.com>
2021-09-26 13:04:37 +0900

    commit for question.

--- MockCocoonApp/MockCocoonApp/ViewController.swift
+++ MockCocoonApp/MockCocoonApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tutorialView: UIView!
+    @IBOutlet weak var templateView: UIView!
+    @IBOutlet var templateButtons: [UIButton]!
+   
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.tutorialView.removeFromSuperview()
+        templateView.bounds = CGRect(x: 16, y: 32, width: self.view.frame.width - 48, height: self.view.frame.height - 56)
+      
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setButtonTitleMargin()
     }
-
-
+    
+    func setButtonTitleMargin() {
+        for button in templateButtons {
+            button.titleEdgeInsets = UIEdgeInsets(top: 8, left: 24, bottom: 8, right: 150)
+        }
+    }
 }
 
