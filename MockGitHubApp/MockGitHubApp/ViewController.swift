9880fb4bd002471a793555c7a40ec15c9abdcdcf
kaito-a-bit <arakai0128@gmail.com>
2021-09-30 12:02:21 +0900

    added images

--- MockGitHubApp/MockGitHubApp/ViewController.swift
+++ MockGitHubApp/MockGitHubApp/ViewController.swift
@@ -10,23 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var myWorksButtons: [UIButton]!
+    
+    @IBOutlet weak var issuesButton: UIButton!
     let imageArr = [
-        UIImage(named: "Issues"),
-        UIImage(named: "PullRequests"),
-        UIImage(named: "Repositories"),
-        UIImage(named: "Organizations")
+        #imageLiteral(resourceName: "Issues"),
+        #imageLiteral(resourceName: "PullRequets"),
+        #imageLiteral(resourceName: "Repositories"),
+        #imageLiteral(resourceName: "Organizations")
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setImagesForButtons()
+        setUpButton()
     }
     
-    func setImagesForButtons() {
-        for button in myWorksButtons {
-            var i = 0
-            button.setImage(imageArr[i], for: .normal)
-            i += 1
+    func setUpButton() {
+        for i in 0...3 {
+            myWorksButtons[i].setImage(imageArr[i], for: .normal)
+            myWorksButtons[i].imageEdgeInsets = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
         }
     }
 }
+
+
