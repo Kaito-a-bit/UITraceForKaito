6b20f3d8bed785f2864b768d3ccab9409a2b68b9
kaito-a-bit <arakai0128@gmail.com>
2021-10-12 15:04:09 +0900

    provisional commit

---
+++ MockInvestApp/MockInvestApp/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  MockInvestApp
+//
+//  Created by 荒井海斗 on 2021/10/12.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MockInvestApp/MockInvestApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MockInvestApp
+//
+//  Created by 荒井海斗 on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
