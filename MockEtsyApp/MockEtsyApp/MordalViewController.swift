25f9e535cf9fb3521f5892ef0088ef88f4ed792a
kaito-a-bit <arakai0128@gmail.com>
2021-10-19 15:03:32 +0900

    initial commit

---
+++ MockEtsyApp/MockEtsyApp/MordalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MordalViewController.swift
+//  MockEtsyApp
+//
+//  Created by 荒井海斗 on 2021/10/19.
+//
+
+import UIKit
+
+class MordalViewController: UIViewController {
+
+    @IBOutlet weak var CancelButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ MockEtsyApp/MockEtsyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockEtsyApp
+//
+//  Created by 荒井海斗 on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
