e3dd61a740044c607104b5061db23dd44f214a51
kaito-a-bit <arakai0128@gmail.com>
2021-10-20 12:37:45 +0900

    added button icons

--- MockEtsyApp/MockEtsyApp/MordalViewController.swift
+++ MockEtsyApp/MockEtsyApp/MordalViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class MordalViewController: UIViewController {
 
-    @IBOutlet weak var CancelButton: UIButton!
+    @IBOutlet weak var continueWithFB: UIButton!
+    @IBOutlet weak var continueWithGoogle: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpFonts()
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func setUpFonts() {
+        continueWithFB.setImage(#imageLiteral(resourceName: "Fb"), for: .normal)
+        continueWithGoogle.setImage(#imageLiteral(resourceName: "chromeIcon"), for: .normal)
+        
+        continueWithFB.imageView?.contentMode = .scaleAspectFit
+        continueWithGoogle.imageView?.contentMode = .scaleAspectFit
+        
+        
+        continueWithFB.imageEdgeInsets = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 200)
+        continueWithGoogle.imageEdgeInsets = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 150)
+//
+        continueWithFB.titleEdgeInsets = UIEdgeInsets(top: 0, left: -100, bottom: 0, right: 0)
+        continueWithGoogle.titleEdgeInsets = UIEdgeInsets(top: 0, left: -100, bottom: 0, right: 0)
     }
     
 
