16b6812bf9c23718eab218a5747ad8142cb30e41
kaito-a-bit <arakai0128@gmail.com>
2021-11-02 13:57:14 +0900

    initial commit for IRL

---
+++ MockIRLApp/MockIRLApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockIRLApp
+//
+//  Created by 荒井海斗 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
